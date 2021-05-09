a09d97b8b39a68994b56db217654856cd5175a01
andres199 <calopez@asertisas.com>
2021-05-09 11:16:40 -0500

    set icon

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BACKEND_URL: 'http://localhost:3001/',
+  BACKEND_URL: 'https://restaurantesllaneros.com:3001/',
   STORAGE_URL: 'https://restaurantesllaneros.com:3602/',
 };
 
