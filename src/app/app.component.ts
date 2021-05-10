cb36c5917643c048951195576b4bd55e7b839f50
andres199 <calopez@asertisas.com>
2021-05-10 09:49:13 -0500

    splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Plugins, StatusBarStyle } from '@capacitor/core';
+const { StatusBar } = Plugins;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+  ngOnInit() {
+    StatusBar.setBackgroundColor({ color: '#795548' });
+  }
 }
