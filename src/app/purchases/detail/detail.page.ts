240d0b167194b8b5cdff64edfe1b4c17f41461ca
andres199 <calopez@asertisas.com>
2021-05-13 15:57:10 -0500

    detalle de orden

--- src/app/purchases/detail/detail.page.ts
+++ src/app/purchases/detail/detail.page.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Venta } from 'src/app/cart/interfaces/venta.interface';
+import { Imagen } from 'src/app/interfaces/imagen.interface';
+import { environment } from 'src/environments/environment';
 import { setPath } from 'src/util/image-path';
 
 @Component({
@@ -9,11 +11,27 @@ import { setPath } from 'src/util/image-path';
 })
 export class DetailPage implements OnInit {
   @Input() compra: Venta;
+
+  paymentSupport: Imagen;
+  showPaymentSupportFull = false;
   constructor() {}
 
   ngOnInit() {
     this.compra.DetalleVenta.forEach((orden) =>
       orden.Producto.Imagenes.forEach((imagen) => setPath(imagen))
     );
+
+    this.paymentSupport = this.compra.soporte_pago;
+  }
+
+  get paymentSupportThumbnail() {
+    return (
+      environment.STORAGE_URL +
+      this.paymentSupport?.path.replace('original', 'pequeno')
+    );
+  }
+
+  get paymentSupportFull() {
+    return environment.STORAGE_URL + this.paymentSupport?.path;
   }
 }
